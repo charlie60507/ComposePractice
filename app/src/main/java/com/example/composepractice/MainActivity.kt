dd9b19fe015d11512a20bc4a593be8815ca32766
Charlie Yang <charlie.yang@gogox.com>
2024-09-08 11:26:13 +0800

    using LazyColumn to replace for-loop adding Greeting

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -51,9 +53,12 @@ fun MyApp(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Greetings(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(10) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
